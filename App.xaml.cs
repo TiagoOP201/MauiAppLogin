f42279d3e767f5b58a2cf155cadb11be1d50d194
TIAGO PACHECO <tiago.pacheco@etec.sp.gov.br>
2024-11-13 11:50:11 -0300

    Add arquvios ao projeto

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,19 @@
         {
             InitializeComponent();
 
+
             MainPage = new AppShell();
         }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+
+        }
     }
+
+    
 }
