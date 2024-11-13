886859fd4e9cf2498535ec753b88b6aecf27e837
TIAGO PACHECO <tiago.pacheco@etec.sp.gov.br>
2024-11-13 14:07:25 -0300

    Modificações

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,9 +5,22 @@
         public App()
         {
             InitializeComponent();
+            string? usuario_logado = null;
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+               if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+                }else
+                {
+                    MainPage = new Protegida();
+                }
+            });
 
 
-            MainPage = new AppShell();
+            
         }
         protected override Window CreateWindow(IActivationState activationState)
         {
--- Login.xaml.cs
+++ Login.xaml.cs
@@ -6,4 +6,53 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			List<DadosUsuario> list_usuarios = new List<DadosUsuario>()
+			{
+				new DadosUsuario()
+				{
+					Usuario = "Jose",
+					Senha = "123" 
+				},
+				new DadosUsuario()
+				{
+					Usuario = "Maria",
+					Senha = "321"
+				}
+				
+
+			};
+
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+            // LINQ
+            bool encontrou = list_usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha));
+
+            if (encontrou)
+            {
+                await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+                App.Current.MainPage = new Protegida();
+
+            }
+            else
+            {
+                throw new Exception("Usuário ou senha incorretos.");
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Fechar");
+        }
+    }
 }
